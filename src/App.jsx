3cc2451f1662b72c77d1cf9e79d627d94250ef96
the-man2004 <heinrichhb111@gmail.com>
2024-06-24 13:18:22 -0800

    Now fetching the places Data from the backend.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useRef, useState, useCallback } from 'react';
+import { useRef, useState, useCallback } from "react";
 
-import Places from './components/Places.jsx';
-import Modal from './components/Modal.jsx';
-import DeleteConfirmation from './components/DeleteConfirmation.jsx';
-import logoImg from './assets/logo.png';
-import AvailablePlaces from './components/AvailablePlaces.jsx';
+import Places from "./components/Places.jsx";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
+import AvailablePlaces from "./components/AvailablePlaces.jsx";
 
 function App() {
   const selectedPlace = useRef();
